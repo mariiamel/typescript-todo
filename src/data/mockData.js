ee9be20ca5393787f84f6137660d8f037db6ecee
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-03-09 17:53:34 -0500

    initial commit: all required modules in package.json all required folders created CSS written Adding README.md to a seperate branch

---
+++ src/data/mockData.js
@@ -0,0 +1,17 @@
+export const mockData = [
+    {
+        name: 'Laundry',
+        dueDate: 0,
+        complete: false
+    },
+    {
+        name: 'Write code',
+        dueDate: 0,
+        complete: false
+    },
+    {
+        name: 'Sleep',
+        dueDate: 0,
+        complete: false
+    }
+]
